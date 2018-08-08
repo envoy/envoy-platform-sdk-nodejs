fccb42057045ae0e7ecd0c7b2cafbc6cb21575ca
Ioan Carol Plangu <info@plangu.com>
2018-08-08 20:33:44 +0200

    standardjs + some cleanup

--- index.js
+++ index.js
@@ -1,83 +1,81 @@
-var Request = require('./lib/request');
-var Response = require('./lib/response');
-var URI = require('urijs');
+var Request = require('./lib/request')
+var Response = require('./lib/response')
+var URI = require('urijs')
 
-function Platform(config) {
-  this.config = config;
-  this._routes = {};
-  this._handlers = {};
-  this._interceptors = {};
+function Platform (config) {
+  this.config = config
+  this._routes = {}
+  this._handlers = {}
+  this._interceptors = {}
 }
 Platform.prototype.handler = function () {
-  var self = this;
+  var self = this
   return function (event, context, callback) {
-    self.res = new Response(self, context);
-    self.req = new Request(self, event, context);
-    self.start_time = process.hrtime();
-    self.event = event;
-    self.context = context;
+    self.res = new Response(self, context)
+    self.req = new Request(self, event, context)
+    self.start_time = process.hrtime()
+    self.event = event
+    self.context = context
     try {
       if (!event.name) {
-        throw new Error("Event issued did not include action.")
+        throw new Error('Event issued did not include action.')
       }
       switch (event.name) {
-      case 'route':
-        self._handleRoute(event, context);
-        break;
-      case 'event':
-        self._handleEvent(event, context);
-        break;
+        case 'route':
+          self._handleRoute(event, context)
+          break
+        case 'event':
+          self._handleEvent(event, context)
+          break
       }
     } catch (e) {
-      self.res.error(e);
+      self.res.error(e)
     }
   }
 }
 Platform.prototype.route = function (name, fn) {
-  this._routes[name] = fn;
+  this._routes[name] = fn
 }
 Platform.prototype._handleRoute = function (event, context) {
-  headers = event.request_meta;
+  let headers = event.request_meta
   if (typeof this._routes[headers.route] !== 'function') {
-    throw new Error("Invalid route configuration.");
+    throw new Error('Invalid route configuration.')
   }
-  var fn = this._routes[headers.route];
-  fn.call(this, this.req, this.res);
+  var fn = this._routes[headers.route]
+  fn.call(this, this.req, this.res)
 }
 Platform.prototype.handle = function (event, fn) {
-  this._handlers[event] = fn;
+  this._handlers[event] = fn
 }
-Platform.prototype.getJobLink = function (path,localhost) {
-  if(!localhost) {
+Platform.prototype.getJobLink = function (path, localhost) {
+  let protocol = 'http'
+  if (!localhost) {
     localhost = 'app.envoy.com'
     protocol = 'https'
-  } else {
-    protocol = 'http'
   }
-  if(!this.req.job) {
-    throw new Error("No job associated with this request.");
+  if (!this.req.job) {
+    throw new Error('No job associated with this request.')
   }
-  if(!this.config.key) {
-    throw new Error("No plugin key in manifest.json.");
+  if (!this.config.key) {
+    throw new Error('No plugin key in manifest.json.')
   }
-  url = URI(path).absoluteTo('/platform/'+this.config.key+'/');
+  let url = URI(path).absoluteTo('/platform/' + this.config.key + '/')
   url.protocol(protocol)
   url.host(localhost)
-  query = url.search(true)
-  query._juuid = this.req.job.id;
+  let query = url.search(true)
+  query._juuid = this.req.job.id
   url.search(query)
-  return url.toString();
+  return url.toString()
 }
 Platform.prototype._handleEvent = function (event, context) {
-  headers = event.request_meta;
+  let headers = event.request_meta
   if (typeof this._handlers[headers.event] !== 'function') {
-    throw new Error("Invalid handler configuration [" + headers.event + "]");
+    throw new Error('Invalid handler configuration [' + headers.event + ']')
   }
-  var fn = this._handlers[headers.event];
-  fn.call(this, this.req, this.res);
+  var fn = this._handlers[headers.event]
+  fn.call(this, this.req, this.res)
 }
 Platform.prototype.intercept = function (event, fn) {
-  this._interceptors[event] = fn;
+  this._interceptors[event] = fn
 }
-module.exports = Platform;
-
+module.exports = Platform

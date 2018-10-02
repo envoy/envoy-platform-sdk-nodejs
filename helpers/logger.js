aa542ed982241c8608d54d42e8a1055b123607f2
carolshark <ioan-carol.plangu@shark.software>
2018-10-02 17:04:57 +0200

    changed logger to output to stdout insteadof stderr (#16)

--- helpers/logger.js
+++ helpers/logger.js
@@ -2,5 +2,7 @@ const debug = require('debug')
 const info = debug('envoy-plugin:info')
 const warn = debug('envoy-plugin:warn')
 const error = debug('envoy-plugin:error')
-
+info.log = console.log.bind(console)
+warn.log = console.log.bind(console)
+error.log = console.log.bind(console)
 module.exports = { info, warn, error }

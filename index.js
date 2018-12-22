03511a56f688c1103a0304dfbb726c00222794e4
Ioan Carol Plangu <info@plangu.com>
2018-12-22 21:44:08 +0100

    added jsdocs to platform file

--- index.js
+++ index.js
@@ -10,8 +10,33 @@ const oauth2Routes = require('./lib/oauth2Routes')
 const get = require('lodash.get')
 const { reportError } = require('./lib/bugsnagHelper')
 
+/**
+ * Envoy platform instance for AWS lambda deployments
+ *
+ * @typedef {Object} Platform
+ * @property {Request} req - Incoming request object
+ * @property {Response} res - Outgoing response object
+ * @property {Sms} sms - Sms helper based on Twilio
+ * @property {Email} email - Email helper based on Mandrill
+ */
 class Platform {
+  /**
+   * @typedef PlatformOpts
+   * @property {string} baseDir - Base dir of the plugin
+   * @property {string} baseUrl - Base url of Envoy API
+  */
+  /**
+   * Envoy platform instance for AWS lambda deployments
+   *
+   * @param {PlatformOpts} config - Configuration object
+   * @example
+   * const platform = new Platform({ baseDir: __dirname })
+   */
   constructor (config) {
+    this.req = null
+    this.res = null
+    this.sms = null
+    this.email = null
     this.config = config || {}
     this.config.key = this.config.key || process.env.ENVOY_PLUGIN_KEY
     this.config.baseUrl = this.config.baseUrl || process.env.ENVOY_BASE_URL || 'https://app.envoy.com'
@@ -29,14 +54,38 @@ class Platform {
     })
   }
 
+  /**
+   * Registers a route handler for a given path
+   *
+   * @param {string} name - url path of the route apearing after /platform/{pluginKey}/
+   * @param {function} fn - function to handle the request type
+   * @example
+   * platform.registerRoute('party/welcome', function (req, res) { res.json({ ok: true }) })
+   */
   registerRoute (name, fn) {
     this._routes[name] = fn
   }
 
+  /**
+   * Registers a worker handler for a given worker name
+   *
+   * @param {string} name - name of the worker
+   * @param {function} fn - function to handle the worker
+   * @example
+   * platform.registerWorker('nda', function (req, res) { res.job_complete('Uploaded', {}) })
+   */
   registerWorker (event, fn) {
     this._workers[event] = fn
   }
 
+  /**
+   * Gets a complete url to a route of the plugin. Forwards job context.
+   *
+   * @param {string} path - relative path of the endpoint
+   * @param {Object.<string, string>} queryParams - query parameters to append
+   * @example
+   * function (req, res) { res.redirect(this.getEventReportLink('/auth')) }
+   */
   getJobLink (path, queryParams = {}) {
     let jobId = get(this.req, 'job.id')
     if (!jobId) {
@@ -45,6 +94,14 @@ class Platform {
     return this.getRouteLink(path, Object.assign(queryParams, { _juuid: jobId }))
   }
 
+  /**
+   * Gets a complete url to a route of the plugin. Forwards event report id.
+   *
+   * @param {string} path - relative path of the endpoint
+   * @param {Object.<string, string>} queryParams - query parameters to append
+   * @example
+   * function (req, res) { res.redirect(this.getEventReportLink('/auth')) }
+   */
   getEventReportLink (path, queryParams = {}) {
     let hubEventId = this.req.event_report_id || this.req.params.event_report_id
     if (!hubEventId) {
@@ -53,6 +110,14 @@ class Platform {
     return this.getRouteLink(path, Object.assign(queryParams, { event_report_id: hubEventId }))
   }
 
+  /**
+   * Gets a complete url to a route of the plugin
+   *
+   * @param {string} path - relative path of the endpoint
+   * @param {Object.<string, string>} queryParams - query parameters to append
+   * @example
+   * function (req, res) { res.redirect(this.getRouteLink('/auth')) }
+   */
   getRouteLink (path, queryParams = {}) {
     if (!this.config.key) {
       throw new Error('No plugin key')
@@ -62,23 +127,62 @@ class Platform {
     return url.toString()
   }
 
+  /**
+   * Updates hub event report
+   *
+   * @param {string} statusSummary - readable description of the status
+   * @param {"in_progress"|"done"|"failed"|"ignored"} eventStatus - status in database format
+   * @param {string} failureReason - readable description of why the job failed
+   * @example
+   * this.eventUpdate('queued') // keeps database status as in_progress
+   */
   async eventUpdate (statusSummary, eventStatus = 'in_progress', failureReason = null) {
     let eventReportId = this.req.event_report_id || this.req.params.event_report_id
     this.api.updateEventReport(eventReportId, statusSummary, eventStatus, failureReason)
   }
 
+  /**
+   * Updates hub event report as complete
+   *
+   * @param {string} statusSummary - readable description of the status
+   * @example
+   * this.eventComplete('Sent')
+   */
   async eventComplete (statusMessage) {
     return this.eventUpdate(statusMessage, 'done')
   }
 
+  /**
+   * Updates hub event report as ignored
+   *
+   * @param {string} statusSummary - readable description of the status
+   * @param {string} failureReason - reason why it was ignored
+   * @example
+   * this.eventIgnored('Not sent', "user_not_found")
+   */
   async eventIgnore (statusMessage, failureReason) {
     return this.eventUpdate(statusMessage, 'ignored', failureReason)
   }
 
+  /**
+   * Updates hub event report as failed
+   *
+   * @param {string} statusSummary - readable description of the status
+   * @param {string} failureReason - reason why it failed
+   * @example
+   * this.eventIgnored('Not sent', "API Error")
+   */
   async eventFail (statusMessage, failureReason) {
     return this.eventUpdate(statusMessage, 'failed', failureReason)
   }
 
+  /**
+   * Gets AWS Lambda compatible handler
+   * @example
+   * const EnvoyPlatform = require('envoy-platform-sdk')
+   * const platformInstance = new EnvoyPlatform()
+   * exports.handler = platformInstance.getHandler()
+   */
   getHandler () {
     return handler.bind(this)
   }
@@ -162,25 +266,3 @@ function getLoggingSignature () {
 }
 
 module.exports = Platform
-
-/*
-class Platform {
-  sms
-  email
-  req
-  res
-
-  constructor() { }
-  registerWorker() { }
-  registerRoute() { }
-
-  getJobLink() { }
-  getEventReportLink() { }
-  getRouteLink() { }
-
-  eventUpdate() { }
-  eventComplete() { }
-  eventIgnore() { }
-  eventFail() { }
-}
-*/

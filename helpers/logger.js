599b0ff2b3172f4d003a9a91125e5d7761549e0e
Ioan Carol Plangu <info@plangu.com>
2018-12-23 19:31:43 +0100

    changed helper files to only include libraries, rather than implement  separate functionality

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,15 +1,1 @@
-const debug = require('debug')
-const info = debug('envoy-plugin:info')
-const warn = debug('envoy-plugin:warn')
-const error = debug('envoy-plugin:error')
-
-function log (...args) {
-  args[0] = args[0]
-    .replace(/^.*envoy-plugin:/, '')
-    .replace(/^([^\s]+)/, `$1 ::`)
-  console.log(...args)
-}
-
-info.log = warn.log = error.log = log
-
-module.exports = { info, warn, error }
+module.exports = require('../lib/logger')
